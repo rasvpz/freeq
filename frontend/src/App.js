6bb72ee749f8047d2342b622cba7c35bf2f82df3
Rashid <rasvpz@gmail.com>
2022-05-28 12:45:31 +0530

    first commit header body footer chakra intialised

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+// import './App.css';
+import Header from './Component/Header/Header'
+import Body from './Component/Body/Body'
+import Footer from './Component/Footer/Footer'
+
+
+function App() {
+  return (
+    <>
+   <Header />
+   <Body />
+   <Footer />
+
+    </>
+  );
+}
+
+export default App;
